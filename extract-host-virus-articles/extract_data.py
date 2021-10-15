4ee2944669b037c5b6c5ffb5464eb87f8ade7cfd
Prashant Gupta <prashantgupta.nz@gmail.com>
2021-10-15 14:30:12 -0700

    Lump Genus common names and other names

--- extract-host-virus-articles/extract_data.py
+++ extract-host-virus-articles/extract_data.py
@@ -65,79 +65,81 @@ if __name__=='__main__':
 	# 		 				'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
 
 
-	# with open('NAm_Rodent_Lit_Search_All_Synonyms.csv') as f:
-	# 	rows = csv.DictReader(f)
-	# 	for row in rows:
-	# 		genus = row['genus.x']
-	# 		genus_syns = row['Genus synonyms']
-	# 		main_common_name = row['Main common name']
-	# 		other_common_name = row['Other common names']
-	# 		excluded_names = row['Excluded names']
-
-	# 		if genus in genus_synonyms:
-	# 			pass
-	# 		else:
-	# 			genus_synonyms[genus] = [genus_syns, main_common_name, other_common_name, excluded_names]
-
-	# with open('NAm_Rodent_Lit_Search_Genus.csv', 'w', newline='') as f:
-	# 	fieldnames = ['genus.x', 'Genus synonyms', 'Main common name', 'Other common names', 'Excluded names']
-	# 	writer = csv.DictWriter(f, fieldnames=fieldnames)
-
-	# 	writer.writeheader()
-	# 	for key, value in genus_synonyms.items():
-	# 		genus_syns = value[0]
-	# 		main_common_name = value[1]
-	# 		other_common_name = value[2]
-	# 		excluded_names = value[3]
-	# 		writer.writerow({'genus.x': key, 'Genus synonyms':genus_syns,'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
-
-
-	with open('NAm_Rodent_Lit_Search_Genus.csv') as f:
+# Create a new file for Genus level queries lumping all main and other common names (per genus)
+	with open('NAm_Rodent_Lit_Search_All_Synonyms.csv') as f:
 		rows = csv.DictReader(f)
 		for row in rows:
 			genus = row['genus.x']
-			synonyms = (row['Genus synonyms'] + '|' + genus).split('|')
-			common_names = [name for name in ((row['Main common name'] + '|' + row['Other common names']).split('|')) if row['Excluded names']=='' or name not in (row['Excluded names'].split('|'))]
-			common_names = [name for name in common_names if name]
-			search_query = '(Virus OR viruses OR viral) AND ('
-			if len(common_names)>0:
-				for name in common_names:
-					search_query += '"' + name + '" OR '
+			genus_syns = row['Genus synonyms']
+			main_common_name = row['Main common name']
+			other_common_name = row['Other common names']
+			excluded_names = row['Excluded names']
 
-			for genus_syn in synonyms:
-				search_query += '"' + genus_syn + '" OR '
-			search_query = search_query[0:-4]
-			search_query += ')'
-			print(search_query)
+			if genus in genus_synonyms:
+				genus_synonyms[genus][1] += "|"+main_common_name
+				genus_synonyms[genus][2] += "|"+other_common_name
+			else:
+				genus_synonyms[genus] = [genus_syns, main_common_name, other_common_name, excluded_names]
 
-			parameters = {
-				'tool':'ASU_BioKIC',
-				'email':'prashant.gupta.2@asu.edu',
-				'api_key':api_key,
-				'db': 'pmc',
-				'term': search_query,
-				'retmax':100000,
-				'retmode':'json'	
-				}
-			response = requests.post('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params=parameters)
-			id_list = response.json()['esearchresult']['idlist']
-			id_list = [int(idx) for idx in id_list]
+	with open('NAm_Rodent_Lit_Search_Genus.csv', 'w', newline='') as f:
+		fieldnames = ['genus.x', 'Genus synonyms', 'Main common name', 'Other common names', 'Excluded names']
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-			print(len(id_list))
+		writer.writeheader()
+		for key, value in genus_synonyms.items():
+			genus_syns = value[0]
+			main_common_name = value[1]
+			other_common_name = value[2]
+			excluded_names = value[3]
+			writer.writerow({'genus.x': key, 'Genus synonyms':genus_syns,'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
 
-			params = {
-				'tool':'ASU_BioKIC',
-				'email':'prashant.gupta.2@asu.edu',
-				'api_key':api_key,
-				'id': id_list,
-				'download':'Y',
-				'format':'ris'
-			}
-			response = requests.get('https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc', params=params)
-			print(response)
-			filename = genus + '.ris'
-			open(filename, 'wb').write(response.content)
-			time.sleep(5)
+
+	# with open('NAm_Rodent_Lit_Search_Genus.csv') as f:
+	# 	rows = csv.DictReader(f)
+	# 	for row in rows:
+	# 		genus = row['genus.x']
+	# 		synonyms = (row['Genus synonyms'] + '|' + genus).split('|')
+	# 		common_names = [name for name in ((row['Main common name'] + '|' + row['Other common names']).split('|')) if row['Excluded names']=='' or name not in (row['Excluded names'].split('|'))]
+	# 		common_names = [name for name in common_names if name]
+	# 		search_query = '(Virus OR viruses OR viral) AND ('
+	# 		if len(common_names)>0:
+	# 			for name in common_names:
+	# 				search_query += '"' + name + '" OR '
+
+	# 		for genus_syn in synonyms:
+	# 			search_query += '"' + genus_syn + '" OR '
+	# 		search_query = search_query[0:-4]
+	# 		search_query += ')'
+	# 		print(search_query)
+
+	# 		parameters = {
+	# 			'tool':'ASU_BioKIC',
+	# 			'email':'prashant.gupta.2@asu.edu',
+	# 			'api_key':api_key,
+	# 			'db': 'pmc',
+	# 			'term': search_query,
+	# 			'retmax':100000,
+	# 			'retmode':'json'	
+	# 			}
+	# 		response = requests.post('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params=parameters)
+	# 		id_list = response.json()['esearchresult']['idlist']
+	# 		id_list = [int(idx) for idx in id_list]
+
+	# 		print(len(id_list))
+
+	# 		params = {
+	# 			'tool':'ASU_BioKIC',
+	# 			'email':'prashant.gupta.2@asu.edu',
+	# 			'api_key':api_key,
+	# 			'id': id_list,
+	# 			'download':'Y',
+	# 			'format':'ris'
+	# 		}
+	# 		response = requests.get('https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc', params=params)
+	# 		print(response)
+	# 		filename = genus + '.ris'
+	# 		open(filename, 'wb').write(response.content)
+	# 		time.sleep(5)
 
 
 			
