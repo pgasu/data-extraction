416a8673290124313b2a3f704824069ecfb1312c
Prashant Gupta <prashantgupta.nz@gmail.com>
2021-10-15 11:23:20 -0700

    download citations as .ris

--- extract-host-virus-articles/extract_data.py
+++ extract-host-virus-articles/extract_data.py
@@ -3,6 +3,7 @@
 import requests
 import os
 import csv
+import time
 
 api_key = '310a232c07175b642551785374fdecd7e508'
 species_synonyms = {}
@@ -64,69 +65,80 @@ if __name__=='__main__':
 	# 		 				'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
 
 
-	with open('NAm_Rodent_Lit_Search_All_Synonyms.csv') as f:
-		rows = csv.DictReader(f)
-		for row in rows:
-			genus = row['genus.x']
-			genus_syns = row['Genus synonyms']
-			main_common_name = row['Main common name']
-			other_common_name = row['Other common names']
-			excluded_names = row['Excluded names']
-
-			if genus in genus_synonyms:
-				pass
-			else:
-				genus_synonyms[genus] = [genus_syns, main_common_name, other_common_name, excluded_names]
-
-	with open('NAm_Rodent_Lit_Search_Genus.csv', 'w', newline='') as f:
-		fieldnames = ['genus.x', 'Genus synonyms', 'Main common name', 'Other common names', 'Excluded names']
-		writer = csv.DictWriter(f, fieldnames=fieldnames)
-
-		writer.writeheader()
-		for key, value in genus_synonyms.items():
-			genus_syns = value[0]
-			main_common_name = value[1]
-			other_common_name = value[2]
-			excluded_names = value[3]
-			writer.writerow({'genus.x': key, 'Genus synonyms':genus_syns,'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
-
-
 	# with open('NAm_Rodent_Lit_Search_All_Synonyms.csv') as f:
 	# 	rows = csv.DictReader(f)
 	# 	for row in rows:
 	# 		genus = row['genus.x']
-	# 		epithet = row['specificEpithet.x']
-	# 		common_names = (row['otherCommonNames'] + '|' + row['mainCommonName']).split('|')
-	# 		genus_synonyms_list = list(set(genus_synonyms[genus] + [genus]))
-	# 		species_synonyms_list = list(set(species_synonyms[genus+' '+epithet] + [epithet]))
+	# 		genus_syns = row['Genus synonyms']
+	# 		main_common_name = row['Main common name']
+	# 		other_common_name = row['Other common names']
+	# 		excluded_names = row['Excluded names']
+
+	# 		if genus in genus_synonyms:
+	# 			pass
+	# 		else:
+	# 			genus_synonyms[genus] = [genus_syns, main_common_name, other_common_name, excluded_names]
+
+	# with open('NAm_Rodent_Lit_Search_Genus.csv', 'w', newline='') as f:
+	# 	fieldnames = ['genus.x', 'Genus synonyms', 'Main common name', 'Other common names', 'Excluded names']
+	# 	writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+	# 	writer.writeheader()
+	# 	for key, value in genus_synonyms.items():
+	# 		genus_syns = value[0]
+	# 		main_common_name = value[1]
+	# 		other_common_name = value[2]
+	# 		excluded_names = value[3]
+	# 		writer.writerow({'genus.x': key, 'Genus synonyms':genus_syns,'Main common name': main_common_name, 'Other common names': other_common_name, 'Excluded names': excluded_names})
+
+
+	with open('NAm_Rodent_Lit_Search_Genus.csv') as f:
+		rows = csv.DictReader(f)
+		for row in rows:
+			genus = row['genus.x']
+			synonyms = (row['Genus synonyms'] + '|' + genus).split('|')
+			common_names = [name for name in ((row['Main common name'] + '|' + row['Other common names']).split('|')) if row['Excluded names']=='' or name not in (row['Excluded names'].split('|'))]
 			
-	# 		search_query = '(Virus OR viruses OR viral) AND ('
-	# 		if len(common_names)>0:
-	# 			for name in common_names:
-	# 				search_query += '"' + name + '" OR '
+			search_query = '(Virus OR viruses OR viral) AND ('
+			if len(common_names)>0:
+				for name in common_names:
+					search_query += '"' + name + '" OR '
 
-	# 		# for genus_syn in genus_synonyms_list:
-	# 		# 	for species_syn in species_synonyms_list:
-	# 		# 		search_query += '"' + genus_syn + ' ' + species_syn + '" OR '
+			for genus_syn in synonyms:
+				search_query += '"' + genus_syn + '" OR '
+			search_query = search_query[0:-4]
+			search_query += ')'
 
-	# 		for genus_syn in genus_synonyms_list:
-	# 			search_query += '"' + genus_syn + '" OR '
-	# 		search_query = search_query[0:-4]
-	# 		search_query += ')'
+			print(search_query)
+			#search_query = '(Virus OR viruses OR viral) AND (Aplodontia rufa OR Aplodontia leporina OR Aplodontia californica OR "Aplodontia major" OR "Aplodontia olympica" OR "Aplodontia pacifica" OR "Aplodontia phaea" OR "Aplodontia rainieri" OR "Aplodontia chryseola" OR "Aplodontia nigra" OR "Aplodontia columbiana" OR "Aplodontia grisea" OR "Aplodontia humboldtiana" OR "Mountain Beaver" OR "Sewellel" OR "Point Arena Mountain Beaver" OR "Point Reyes Mountain Beaver" OR "Haploodus nigra" OR "Haploodus leporina" OR "Haploodus phaea" OR "Haploodus olympica" OR "Haploodus humboldtiana" OR "Haploodus rufa" OR "Haploodus grisea" OR "Haploodus major" OR "Haploodus chryseola" OR "Haploodus californica" OR "Haploodus columbiana" OR "Haploodus pacifica" OR "Haploodus rainieri")'
+			parameters = {
+				'tool':'ASU_BioKIC',
+				'email':'prashant.gupta.2@asu.edu',
+				'api_key':api_key,
+				'db': 'pmc',
+				'term': search_query,
+				'retmax':100000,
+				'retmode':'json'	
+				}
+			response = requests.post('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params=parameters)
+			print(response.json()['esearchresult']['idlist'])
 
-	# 		print(search_query)
-	# 		#search_query = '(Virus OR viruses OR viral) AND (Aplodontia rufa OR Aplodontia leporina OR Aplodontia californica OR "Aplodontia major" OR "Aplodontia olympica" OR "Aplodontia pacifica" OR "Aplodontia phaea" OR "Aplodontia rainieri" OR "Aplodontia chryseola" OR "Aplodontia nigra" OR "Aplodontia columbiana" OR "Aplodontia grisea" OR "Aplodontia humboldtiana" OR "Mountain Beaver" OR "Sewellel" OR "Point Arena Mountain Beaver" OR "Point Reyes Mountain Beaver" OR "Haploodus nigra" OR "Haploodus leporina" OR "Haploodus phaea" OR "Haploodus olympica" OR "Haploodus humboldtiana" OR "Haploodus rufa" OR "Haploodus grisea" OR "Haploodus major" OR "Haploodus chryseola" OR "Haploodus californica" OR "Haploodus columbiana" OR "Haploodus pacifica" OR "Haploodus rainieri")'
-	# 		parameters = {
-	# 			'tool':'ASU_BioKIC',
-	# 			'email':'prashant.gupta.2@asu.edu',
-	# 			'api_key':api_key,
-	# 			'db': 'pmc',
-	# 			'term': search_query,
-	# 			'retmax':100000,
-	# 			'retmode':'json'	
-	# 			}
-	# 		response = requests.post('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params=parameters)
-	# 		print(len(response.json()['esearchresult']['idlist']))
+			id_list = response.json()['esearchresult']['idlist']
+
+			ids = [int(idx) for idx in id_list]
+			print(ids)
+
+			params = {
+				'id': ids,
+				'download':'Y',
+				'format':'ris'
+			}
+			response = requests.get('https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc', params=params)
+			print(response)
+			filename = genus + '.ris'
+			open(filename, 'wb').write(response.content)
+			time.sleep(1)
+
 
 			
 			
