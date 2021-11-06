253da28562acf4552cce8991460e2c5481c4f3dc
Prashant Gupta <prashantgupta.nz@gmail.com>
2021-11-05 17:19:55 -0700

    remove excluded terms from genus, species and scientific names

--- extract-host-virus-articles/query.py
+++ extract-host-virus-articles/query.py
@@ -126,7 +126,7 @@ def create_scientific_name_union_common_names_query(row):
 	genus = row['genus.x']
 	epithet = row['specificEpithet.x']
 	genus_synonyms = [syn for syn in row['Genus synonyms'].split('|') if row['Excluded names']=='' or syn not in (row['Excluded names'].split('|'))]
-	epithet_synonyms = row['Epithet synonyms'].split('|')
+	epithet_synonyms = [syn for syn in row['Epithet synonyms'].split('|') if row['Excluded names']=='' or syn not in (row['Excluded names'].split('|'))]
 	common_names = row['Main common name'] if row['Other common names']=="" else row['Main common name'] + '|' + row['Other common names']
 	common_names = [name for name in common_names.split('|') if row['Excluded names']=='' or name not in (row['Excluded names'].split('|'))]
 
@@ -138,7 +138,8 @@ def create_scientific_name_union_common_names_query(row):
 
 	for genus_syn in genus_synonyms:
 		for epithet_syn in epithet_synonyms:
-			search_query +=  '"' + genus_syn + ' ' + epithet_syn + '" OR '
+			if row['Excluded names']=='' or genus_syn + ' ' + epithet_syn not in (row['Excluded names'].split('|')):
+				search_query +=  '"' + genus_syn + ' ' + epithet_syn + '" OR '
 
 	search_query = search_query[0:-4] + ')'
 	return search_query
@@ -174,6 +175,27 @@ def decompose_scientific_name_union_common_names_query(row):
 			list_of_search_queries.append(search_query)
 
 	return list_of_search_queries
+
+def create_scientific_name_intersection_common_names_query(row):
+	genus = row['genus.x']
+	epithet = row['specificEpithet.x']
+	genus_synonyms = [syn for syn in row['Genus synonyms'].split('|') if row['Excluded names']=='' or syn not in (row['Excluded names'].split('|'))]
+	epithet_synonyms = row['Epithet synonyms'].split('|')
+	common_names = row['Main common name'] if row['Other common names']=="" else row['Main common name'] + '|' + row['Other common names']
+	common_names = [name for name in common_names.split('|') if row['Excluded names']=='' or name not in (row['Excluded names'].split('|'))]
+
+	search_query = '("Virus" OR "viruses" OR "viral") AND ('
+
+	if len(common_names)>0:
+		for name in common_names:
+			search_query += '"' + name + '" OR '
+
+	for genus_syn in genus_synonyms:
+		for epithet_syn in epithet_synonyms:
+			search_query +=  '"' + genus_syn + ' ' + epithet_syn + '" OR '
+
+	search_query = search_query[0:-4] + ')'
+	return search_query
 
 
 
